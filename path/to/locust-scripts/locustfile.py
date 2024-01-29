c6bbce7f2d94520a956badc48981df620a04baf5
João Batista Romao <j.batista.romao@gmail.com>
2024-01-29 20:31:10 -0300

    adicionando e configurando locust no docker compose

---
+++ path/to/locust-scripts/locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+
+class MyUser(HttpUser):
+    wait_time = between(1, 3)  # Aguarda entre 1 e 3 segundos entre as tarefas
+
+    @task
+    def visit_homepage(self):
+        self.client.get("/")
+
+    @task
+    def visit_about_page(self):
+        self.client.get("/about")
