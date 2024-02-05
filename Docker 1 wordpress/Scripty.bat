f0b22a29fb6580eb7810753239329a28dc4e5930
João Batista Romao <j.batista.romao@gmail.com>
2024-02-05 01:34:30 -0300

    Ajustando scriptys para mais otimizados

--- Docker 1 wordpress/Scripty.bat
+++ Docker 1 wordpress/Scripty.bat
@@ -1,27 +1,39 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+:: Definindo os valores do array
+set array[0]=100
+set array[1]=200
+set array[2]=300
+set array[3]=400
+set array[4]=500
 
 rem Definir uma variável de ambiente para armazenar a hora atual
 for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
 
 rem Formatar a hora atual para incluir apenas caracteres válidos em nomes de diretórios
-set formatted_datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%_%datetime:~8,2%-%datetime:~10,2%
+set formatted_datetime=!datetime:~0,4!-!datetime:~4,2!-!datetime:~6,2!_!datetime:~8,2!-!datetime:~10,2!
 
-rem Iniciar o Docker Compose
-docker-compose up -d
+:: Acessando e exibindo os valores do array
+for /l %%i in (0,1,4) do (
+    set /a numeroDeUsuarios=!array[%%i]!
 
-rem Aguardar alguns segundos para garantir que os serviços estejam totalmente iniciados
-timeout /t 10
+    rem Iniciar o Docker Compose
+    docker-compose up -d
 
-rem Iniciar a tarefa no Locust com os parâmetros especificados
-locust -f .\path\to\locust-scripts\locustfile.py --headless -u 100 -r 10 -t 1m --csv result --host=http://localhost:80
+    rem Aguardar alguns segundos para garantir que os serviços estejam totalmente iniciados
+    timeout /t 10
 
-rem Criar uma pasta com o nome formatado que inclui a hora atual
-mkdir analysis_results_%formatted_datetime%
+    rem Iniciar a tarefa no Locust com os parâmetros especificados
+    locust -f .\path\to\locust-scripts\locustfile.py --headless -u !numeroDeUsuarios! -r 10 -t 5m --csv result --host=http://localhost:80
 
-rem Mover os arquivos de análise para a pasta criada
-move .\*.csv analysis_results_%formatted_datetime%\
+    rem Renomear e mover os arquivos de análise para a pasta criada com o número de usuários no nome
+    set result_path=analysis_results_!formatted_datetime!_!numeroDeUsuarios!_user
+    mkdir !result_path!
+    move .\result_stats.csv !result_path!\result_stats_!numeroDeUsuarios!.csv
 
-rem Encerrar os serviços do Docker Compose
-docker-compose down
+    rem Encerrar os serviços do Docker Compose
+    docker-compose down
+)
 
-@echo off
+endlocal
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Users\jbati\Documents\Docker\Docker 1 wordpress\Scripty.bat"
+"C:\Users\jbati\Documents\Docker\Docker 2 wordpress\Scripty.bat"
+"C:\Users\jbati\Documents\Docker\Docker 3 wordpress\Scripty.bat"
